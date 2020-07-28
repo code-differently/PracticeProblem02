ffe45ce4fffa3bc2c78be13eca82f475a968270c
Dominic Davis <dompc6789@gmail.com>
2020-07-28 15:36:26 -0400

    made my attempt at problem 2

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,13 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+    //I suppose I start with an Array here?
+     public int[] organizeArray(int[] firstarray){
+         Arrays.sort(firstarray);
+         
+         
+         return null;
+     }
 }
