f0d1b98d9b09566d28782ce97635e7408f20e933
girlswhocodee <myekini1@umbc.edu>
2020-07-28 15:22:23 -0400

    Continuous Subarray Attempt

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,43 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    //method to find if elements are continuous
+    public boolean isContinuous(int num1, int num2){
+        if(Math.abs(num1 - num2) != 1){
+            return false;
+        } else
+            {
+            return true;
+            }
+    }
+
+        
+    //find min and max of elements, original idea
+        int min(int x, int y){
+            return (x < y) ? x : y;
+        }
+
+        int max(int x, int y){
+            return (x > y) ? x : y;
+        }
+
+    public int findLongestSubarray(int[] arr){
+
+        Arrays.sort(arr);
+       
+        int maxLength = 1;
+        int count = 0;
+
+        for(int i = 0; i < arr.length -1; i++){
+            count++;
+            if(count > maxLength){
+                maxLength = count;
+            }
+           
+        }
+        return maxLength;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,44 @@
 package com.stayready;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class Problem02Test {
+
+    @Test
+    public void isContinuousTest(){
+        //Given
+        Problem02 problem02 = new Problem02();
+        //When
+        int num1 = 8;
+        int num2 = 9;
+        //Then 
+        assertTrue(problem02.isContinuous(num1, num2));
+    }
+
+    @Test
+    public void isContinuousTest2(){
+        //Given
+        Problem02 problem02 = new Problem02();
+        //When 
+        int num1 = 7;
+        int num2 = 11;
+        //Then
+        assertFalse(problem02.isContinuous(num1, num2));
+    }
+
+
+    @Test
+    public void longestContSubarrayTest(){
+    //Given 
+    Problem02 problem02 = new Problem02();
+    int[] arr = {10, 12, 11};
+    //When
+    int expected = 2;
+    //Then
+    assertEquals(expected, problem02.findLongestSubarray(arr));
+    }
 }
