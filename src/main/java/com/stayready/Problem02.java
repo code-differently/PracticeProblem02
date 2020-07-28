25ad81cbe72ac214c59a00407e3408972b4790c8
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-28 14:28:29 -0400

    finished

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,35 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    public boolean isContiguous (int num1, int num2) {
+        if (Math.abs(num1 - num2) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int longestContiguousSubArray(int[] nums) {
+        Arrays.sort(nums);
+        int currentCount = 1;
+        int maxCount = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            if (isContiguous(nums[i], nums[i - 1])) {
+                currentCount++;
+            } else {
+                currentCount = 1;
+            }
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+            }
+        }
+        return maxCount;
+    }
+
+    public String displayOutput(int num) {
+        return "Length of the longest contiguous subarray is " + num;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,87 @@
 package com.stayready;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class Problem02Test {
+    Problem02 obj = new Problem02();
+    String message = "Length of the longest contiguous subarray is ";
+
+    @Test
+    public void isContiguousTest() {
+        int num1 = 5;
+        int num2 = 6;
+
+        assertTrue(obj.isContiguous(num1, num2));
+    }
+
+    @Test
+    public void isContiguousTest2() {
+        int num1 = 5;
+        int num2 = 6;
+
+        assertTrue(obj.isContiguous(num2, num1));
+    }
+
+    @Test
+    public void isContiguousTest3() {
+        int num1 = 10;
+        int num2 = 6;
+
+        assertFalse(obj.isContiguous(num1, num2));
+    }
+
+    @Test
+    public void longestContiguousSubArrayTest() {
+        int[] nums = {10, 12, 11};
+
+        int expected = 3;
+
+        assertEquals(expected, obj.longestContiguousSubArray(nums));
+    }
+
+    @Test
+    public void longestContiguousSubArrayTest2() {
+        int[] nums = {14, 12, 11, 20};
+
+        int expected = 2;
+
+        assertEquals(expected, obj.longestContiguousSubArray(nums));
+    }
+
+    @Test
+    public void longestContiguousSubArrayTest3() {
+        int[] nums = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+
+        int expected = 5;
+
+        assertEquals(expected, obj.longestContiguousSubArray(nums));
+    }
+
+    @Test
+    public void displayOutput() {
+        int[] nums = {10, 12, 11};
+
+        String expected = message += "3";
+
+        assertEquals(expected, obj.displayOutput(obj.longestContiguousSubArray(nums)));
+    }
+
+    @Test
+    public void displayOutput2() {
+        int[] nums = {14, 12, 11, 20};
+
+        String expected = message += "2";
+
+        assertEquals(expected, obj.displayOutput(obj.longestContiguousSubArray(nums)));
+    }
+
+    @Test
+    public void displayOutput3() {
+        int[] nums = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+
+        String expected = message += "5";
+
+        assertEquals(expected, obj.displayOutput(obj.longestContiguousSubArray(nums)));
+    }
 }
