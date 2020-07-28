7c029702b5e6684fa750e8313f8d8e1e51847a9f
Aldo <aalmeidarojo18@students.desu.edu>
2020-07-28 15:43:50 -0400

    Completed

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,29 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    static String getContinuousSubarray(int[] array){
+        Arrays.sort(array); // Sort Array from least to greatest
+        int largestCount=0; // Hold largest number of continuous numbers
+        if(array.length>0) {
+            int num=array[0]; // Number to check if continuous
+            int count=1; //Always count itself then start looping after first element
+            for (int i = 1; i < array.length; i++) {
+                if(array[i]==num+1) {
+                    count++;
+                }
+                else {
+                    if(count>largestCount)
+                        largestCount=count;
+                    count=1;
+                }
+                num=array[i];
+            }
+            if(count>largestCount)
+                largestCount=count;
+        }
+        return("Length of the longest contiguous subarray is "+largestCount);
+    }
 }
