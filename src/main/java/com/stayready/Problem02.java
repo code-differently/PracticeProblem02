eabbd407ec15fed49a6fa45fd60a1c4f3b616e06
beastlyCoder <imaaronacs@gmail.com>
2020-07-28 10:53:39 -0700

    doneeee

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,34 @@
 package com.stayready;
 
-public class Problem02 {
+import java.util.Arrays;
+
+public class Problem02
+{
+    public static int longestContiguous(int[] arr)
+    {
+
+        if(arr.length == 0)
+        {
+            return 0;
+        }
+        Arrays.sort(arr);
+        int tempSequence = 1;
+        int lenSequence = 1;
+
+
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            if(arr[i] < arr[i + 1] && Math.abs(arr[i] - arr[i + 1]) == 1)
+            {
+                tempSequence++;
+                lenSequence = Math.max(lenSequence, tempSequence);
+            }
+            else
+            {
+                tempSequence = 1;
+            }
+        }
+
+        return lenSequence;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,24 @@
 package com.stayready;
 
-public class Problem02Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem02Test
+{
+    @Test
+    public void testSequence()
+    {
+        int[] seq = {10, 12, 11};
+        int len = 3;
+
+        int[] seq2 = {14, 12, 11, 20};
+        int len2 = 2;
+
+        int[] seq3 = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+        int len3 = 5;
+
+        Assert.assertEquals(len, new Problem02().longestContiguous(seq));
+        Assert.assertEquals(len2, new Problem02().longestContiguous(seq2));
+        Assert.assertEquals(len3, new Problem02().longestContiguous(seq3));
+    }
 }
