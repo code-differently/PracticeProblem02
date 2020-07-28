4ccef188433d1d55036f2a8049faab0df62049d4
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-28 14:36:39 -0500

    Problem2 Attempt

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,36 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    public Integer[] sort(Integer[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
+    Integer minimum(Integer x, Integer y) {
+        Integer min = 0;
+        if( x > y) {
+            min = y;
+        } else {
+            min = x;
+        }
+        return min;
+    }
+
+
+    public Integer longest(Integer[] input) {
+        int maxL = 1 ;
+        int num = 0;
+        Arrays.sort(input);
+        
+        for(int i = 0; i < input.length - 1; i++) {
+            num ++;
+            if(num > maxL) {
+                maxL = num;
+            }
+          }
+        return maxL;
+    }
 }
