ed8b93eae321794cf22d7cac51e2df2ad19d9044
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-28 18:49:15 -0400

    pushing my effort

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,27 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    public int[] sortArray(int[] sortedArray) {
+        Arrays.sort(sortedArray);
+        return sortedArray;
+    }
+
+    int Contiguous(int[] arr) {
+        int[] sortedArray = sortArray(arr);
+        int currNum = 0; // declare current number
+        int count = 0;
+
+        for (int i = 0; i < sortedArray.length; i++) {
+            if (i == 0) {
+                count = 1;
+
+            }
+
+        }
+        return 0;
+    }
+
 }
