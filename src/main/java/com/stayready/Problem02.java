ea7fdefa76714aae62492ba1d8d3dffb9e789d5f
mercelisvaughan <mercelis.vaughan51@gmail.com>
2020-07-28 15:03:59 -0400

    Finished

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,29 @@
 package com.stayready;
+import java.util.Arrays;
 
 public class Problem02 {
+
+    public Integer countContinuousNumbers(Integer[] arr){
+        Integer count = 1;
+        Integer longestSub = count;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            Integer nextPosition = arr[i + 1] - 1;
+            if (arr[i] == nextPosition){
+                count++;
+                longestSub = count;
+            }
+            else {
+                count = 1;
+            }
+        }
+
+        if (longestSub > count){
+            return longestSub;
+        }
+        else {
+            return count;
+        }
+    }
 }
