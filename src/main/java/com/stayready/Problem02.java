85de9af4c238849ca4ac31ef4c4f1f52bdbebec5
esanvee <eesanvee18@students.desu.edu>
2020-07-28 14:56:06 -0400

    Done

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,69 @@
 package com.stayready;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.plaf.synth.SynthFormattedTextFieldUI;
+
 public class Problem02 {
+
+    /*Game plan
+
+    sort the array into arraylist
+
+    with each consecutive step it increases count
+
+    if the number is bad, save and reset count
+
+    */
+
+
+
+    public static List<Integer> sort(Integer[] ary){
+
+        List<Integer> sortedList = Arrays.asList(ary);
+
+        Collections.sort(sortedList);
+
+        return sortedList;
+
+    }
+
+    public static int findChain(Integer[] ary){
+
+        List<Integer> sortedList = sort(ary); //Sort and convert to arylist
+
+        int chainCnt = 0;
+        int maxChain = 0; //registers when chain is broken
+
+        for(int i = 0; i < sortedList.size()-1; i++){
+            System.out.println(maxChain);
+
+            //If next value equals current value + 1
+            if((sortedList.get(i))+1 == sortedList.get(i+1)){
+
+                chainCnt++;
+
+                if(chainCnt + 1 > maxChain){ //new record
+
+                    maxChain = chainCnt + 1;
+                    
+
+                }
+
+                
+            }else{
+                
+                chainCnt = 0; //reset
+            }
+
+        }
+
+
+
+
+        return maxChain;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,95 @@
 package com.stayready;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem02Test {
+
+    Problem02 problem02;
+
+
+
+
+    @Test
+
+    public void testSort(){
+
+        Integer[] test = {1,4,5,9,0,5};
+
+        Integer[] sortedTest = {0,1,4,5,5,9};
+
+
+        List <Integer> expectedList = new ArrayList<>();
+        expectedList.addAll(Arrays.asList(sortedTest));
+
+        List<Integer> actualList = Problem02.sort(test);
+
+        Assert.assertEquals(expectedList, actualList);
+
+
+    }
+
+    @Test
+
+    public void testChain(){
+
+        Integer[] test = {10, 12, 11};
+
+        int expectedChain = 3;
+        int actualChain = Problem02.findChain(test);
+
+        Assert.assertEquals(expectedChain, actualChain);
+
+
+
+    }
+
+    @Test
+
+    public void testChain2(){
+
+        Integer[] test = {14, 12, 11, 20};
+
+        int expectedChain = 2;
+        int actualChain = Problem02.findChain(test);
+
+        Assert.assertEquals(expectedChain, actualChain);
+
+
+
+    }
+
+    @Test
+
+    public void testChain3(){
+
+        Integer[] test = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+
+        int expectedChain = 5;
+        int actualChain = Problem02.findChain(test);
+
+        Assert.assertEquals(expectedChain, actualChain);
+
+
+
+    }
+
+    @Test
+
+    public void testChain4(){
+
+        Integer[] test = {21, 36, 58, 47, 90, 82, 94, 3, 61, 75};
+
+        int expectedChain = 0;
+        int actualChain = Problem02.findChain(test);
+
+        Assert.assertEquals(expectedChain, actualChain);
+
+
+
+    }
 }
