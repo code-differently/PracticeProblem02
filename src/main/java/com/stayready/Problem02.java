4a6b7953b7c757b3833d75f04253859c50a660b2
Giovane Barnes <gjbarnes19@students.desu.edu@Giovanes-MacBook-Pro.local>
2020-07-28 17:06:39 -0400

    Attempt

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,27 @@
 package com.stayready;
+import java.util.Arrays;
+import java.lang.Integer;
+
 
 public class Problem02 {
+
+    public int greatestConsecutiveSequence(int[] arr) {
+
+            int ans = 0, count = 0;
+
+            Arrays.sort(arr);
+            
+            for (int i = 0; i < arr.length; i++) {
+
+                if (i > 0 && arr[i - 1] == arr[i] - 1)
+                    count++;
+                else
+                    count = 1;
+
+                ans = Integer.max(ans, count);
+            }
+            return ans;
+        }
+
+
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,61 @@
 package com.stayready;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Problem02Test {
+
+    @Test
+    public void greatestConsecutiveSequenceTest(){
+        int [] arr = {10, 12, 11};
+
+        Problem02 tester = new Problem02();
+
+        int expected = 3;
+        int actual = tester.greatestConsecutiveSequence(arr);
+
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void greatestConsecutiveSequenceTest2(){
+        int [] arr =  {14, 12, 11, 20};
+
+        Problem02 tester = new Problem02();
+
+        int expected = 2;
+        int actual = tester.greatestConsecutiveSequence(arr);
+
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void greatestConsecutiveSequenceTest3(){
+        int [] arr = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+
+        Problem02 tester = new Problem02();
+
+        int expected = 5;
+        int actual = tester.greatestConsecutiveSequence(arr);
+
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void greatestConsecutiveSequenceTest4(){
+        int [] arr = {1};
+
+        Problem02 tester = new Problem02();
+
+        int expected = 1;
+        int actual = tester.greatestConsecutiveSequence(arr);
+
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
 }
