ec9355e8beeea66a28b024c41fecf538b631eb37
IsaiahB1 <isaiahbarnett16@yahoo.com>
2020-07-28 17:07:39 -0500

    test are done, nothing in the main.

--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -17,8 +17,8 @@ public class Problem02Test {
         int[] sequence3 = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
         int length3 = 5;
         //Then
-        Assert.assertEquals(length, new Problem02Test().longestContiguous(sequence));
-        Assert.assertEquals(length2, new Problem02Test().longestContiguous(sequence2));
-        Assert.assertEquals(length3, new Problem02Test().longestContiguous(sequence3));
+        Assert.assertEquals(length, new Problem02Test().sequenceTest(sequence));
+        Assert.assertEquals(length2, new Problem02Test().sequenceTest(sequence2));
+        Assert.assertEquals(length3, new Problem02Test().sequenceTest(sequence3));
     }
 }
