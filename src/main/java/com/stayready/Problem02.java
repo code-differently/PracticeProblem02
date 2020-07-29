69560eb362fcb6b747f6fc98c33152f078643c3a
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-28 20:45:14 -0400

    completed problem 02

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,32 @@
 package com.stayready;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Problem02 {
+
+    public static String continuosSequence(int[] array){
+        Arrays.sort(array);
+        int count = 1;
+        int maxCount = 0;
+
+        for(int i=0; i<array.length-1; i++){
+            if(array[i+1] == array[i]+1){
+                count++;
+            }else{
+                if(count > maxCount)
+                    maxCount = count;
+                    count = 1;
+            }
+        }if(count > maxCount)
+            maxCount = count;
+        return "Length of the longest contiguous subarray is "+ maxCount;
+    }
+
+//    private int Compare(int max, int count){
+//        if(count > max)
+//            max = count;
+//        return max;
+//    }
 }
