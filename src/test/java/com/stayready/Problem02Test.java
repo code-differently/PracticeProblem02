036aff9751188c2bb39830b2751636a9d775fa34
IsaiahB1 <isaiahbarnett16@yahoo.com>
2020-07-28 20:01:45 -0500

    Completed

--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -6,19 +6,36 @@ import org.junit.Assert;
 public class Problem02Test {
 
     @Test
-    public void sequenceTest(){
+    public void Test1(){
         //Given
-        int[] sequence = {10, 12, 11};
-        int length = 3;
+        int arr[] = {10, 12, 11};
 
-        int[] sequence2 = {14, 12, 11, 20};
-        int length2 = 2;
-
-        int[] sequence3 = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
-        int length3 = 5;
+        String expected = "Length of the longest contiguous subarray is 3";
+        String actual = Problem02.continuosSequence(arr);
         //Then
-        Assert.assertEquals(length, new Problem02Test().sequenceTest(sequence));
-        Assert.assertEquals(length2, new Problem02Test().sequenceTest(sequence2));
-        Assert.assertEquals(length3, new Problem02Test().sequenceTest(sequence3));
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2(){
+        int arr[] = {14, 12, 11, 20};
+
+        String expected = "Length of the longest contiguous subarray is 2";
+        String actual = Problem02.continuosSequence(arr);
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void Test3(){
+        int arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+
+        String expected = "Length of the longest contiguous subarray is 5";
+        String actual = Problem02.continuosSequence(arr);
+
+        Assert.assertEquals(expected, actual);
+
     }
 }
