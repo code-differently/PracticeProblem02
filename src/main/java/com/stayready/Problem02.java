d83f8dcf27876a6f29e298ebba2593c7501c1251
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-28 22:04:40 -0400

    test 3 failure

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -4,24 +4,25 @@ import java.util.Arrays;
 
 public class Problem02 {
 
-    public int[] sortArray(int[] sortedArray) {
-        Arrays.sort(sortedArray);
-        return sortedArray;
-    }
+    public static String Contiguous(int[] arr) {
+        Arrays.sort(arr);
+        int count =  1; // declare counter
+        int maxCount = 0; // declare current number
+        String result = "Length of the longest contiguous subarray is ";
 
-    int Contiguous(int[] arr) {
-        int[] sortedArray = sortArray(arr);
-        int currNum = 0; // declare current number
-        int count = 0;
-
-        for (int i = 0; i < sortedArray.length; i++) {
-            if (i == 0) {
-                count = 1;
-
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i+1] == arr[i]+1) {
+                count++;
+            } else {
+                if (count > maxCount){
+                    maxCount = count;
+                    count = 1;
+                }
             }
-
         }
-        return 0;
+        if  (count > maxCount) {
+            maxCount = count;
+        }
+        return result + maxCount;
     }
-
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -4,55 +4,35 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class Problem02Test {
-
-    Problem02 problem;
-
-    @Before
-    public void setup() {
-
-        this.problem = new Problem02();
-    }
-
-    @Test
-    public void sortArrayTest() {
-        int[] arr = { 1, 3, 2, 4 };
-        int[] expected = { 1, 2, 3, 4 };
-
-        int[] actual = problem.sortArray(arr);
-
-        Assert.assertArrayEquals(expected, actual);
-    }
 
     @Test
     public void testOutput1() {
-        int arr[] = { 10, 12, 11 };
-        int expected = 3;
+        int[] arr = { 10, 12, 11 };
 
-        Integer len = arr.length;
-        int actual = problem.Contiguous(arr);
+        String expected = "Length of the longest contiguous subarray is 3";
+        String actual = Problem02.Contiguous(arr);
 
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testOutput2() {
-        int arr[] = { 14, 12, 11, 20 };
-        int expected = 2;
+        int[] arr = { 14, 12, 11, 20 };
 
-        Integer len = arr.length;
-        int actual = problem.Contiguous(arr);
+        String expected = "Length of the longest contiguous subarray is 2";
+        String actual = Problem02.Contiguous(arr);
 
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testOutput3() {
-        int arr[] = { 1, 56, 58, 57, 90, 92, 94, 93, 91, 45 };
-        int expected = 5;
+        int[] arr = { 1, 56, 58, 57, 90, 92, 94, 93, 91, 45 };
 
-        Integer len = arr.length;
-        int actual = problem.Contiguous(arr);
+        String expected = "Length of the longest contiguous subarray is 5";
+        String actual = Problem02.Contiguous(arr);
 
         Assert.assertEquals(expected, actual);
     }
